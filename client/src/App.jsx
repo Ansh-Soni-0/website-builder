133445da0e91af42d5efb1e4102ea6b24dc09810
ansh soni <anshsoni200002@gmail.com>
2026-08-04 10:33:51 +0530

    created home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,45 @@
-  import React from 'react'
-  import { Routes , Route, Navigate} from 'react-router-dom'
-  import AuthPage from './pages/AuthPage'
-  import { AuthLayout, GuestLayout } from './pages/Layout'
-  import HomePage from './pages/HomePage'
-  import BuilderPage from './pages/BuilderPage'
-  import PreviewPage from './pages/PreviewPage'
+import React from 'react'
+import { Routes , Route, Navigate} from 'react-router-dom'
+import AuthPage from './pages/AuthPage'
+import { AuthLayout, GuestLayout } from './pages/Layout'
+import HomePage from './pages/HomePage'
+import BuilderPage from './pages/BuilderPage'
+import PreviewPage from './pages/PreviewPage'
+import { Toaster } from 'react-hot-toast'
 
   const App = () => {
     return (
-      <Routes>
-        {/* login routes  */}
-        <Route element={<GuestLayout/>}>
+      <>
+        <Toaster />
+        <Routes>
+          {/* login routes  */}
+          <Route element={<GuestLayout/>}>
 
-          <Route path='/login' element={<AuthPage mode="login"/>} />
-          <Route path='/register' element={<AuthPage mode="register"/>} />
+            <Route path='/login' element={<AuthPage mode="login"/>} />
+            <Route path='/register' element={<AuthPage mode="register"/>} />
 
-        </Route>
+          </Route>
 
-        {/* Protected routes routes  */}
-        <Route element={<AuthLayout/>}>
+          {/* Protected routes routes  */}
+          <Route element={<AuthLayout/>}>
 
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/builder/:id' element={<BuilderPage/>} />
-          <Route path='/preview/:id' element={<PreviewPage/>} />
-          
-          
+            <Route path='/' element={<HomePage/>} />
+            <Route path='/builder/:id' element={<BuilderPage/>} />
+            <Route path='/preview/:id' element={<PreviewPage/>} />
+            
+            
 
-        </Route>
-
-
-        {/* catch all  */}
-        <Route path='*' element={<Navigate to="/" replace />} />
+          </Route>
 
 
+          {/* catch all  */}
+          <Route path='*' element={<Navigate to="/" replace />} />
 
 
-      </Routes>
+
+
+        </Routes>
+      </>
     )
   }
 
